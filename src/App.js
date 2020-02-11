6d27fb42a8e968be26c9617e7173338136e2af76
koyo-miyamura <nexus9071@gmail.com>
2020-02-12 03:26:28 +0900

    クリップボードにコピーしてみた

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import './App.css'
+import Clipboard from "clipboard"
 
 function App() {
   const [ctx, setCtx] = useState(null)
   const [view, setView] = useState(null)
 
+  const canvasWidth = 800
+  const canvasHeight = canvasWidth * 9 / 16
+  const fontSizePx = 80
+  const fontColor = '#FFFFFF'
+
   useEffect(() => {
     const view = document.querySelector('.canvas')
-    view.width = 1000
-    view.height = 1000 * (window.innerHeight / window.innerWidth)
+    view.width = canvasWidth
+    view.height = canvasHeight
 
     setView(view)
     setCtx(view.getContext('2d'))
-  }, [])
+  }, [canvasHeight])
 
   const handleUploadImage = (e) => {
     ctx.clearRect(0, 0, view.width, view.height)
@@ -22,20 +28,40 @@ function App() {
         const image = new Image()
         image.src = reader.result
         image.onload = () => {
-          ctx.drawImage(image, 0, 0)
+          ctx.drawImage(image, 0, 0, canvasWidth, canvasHeight)
+          ctx.save()
+          ctx.font = `bolder ${fontSizePx}px 'MS Pゴシック'`
+          ctx.textAlign = 'center'
+          ctx.fillStyle = fontColor
+          ctx.fillText('L G T M', canvasWidth/2, canvasHeight/2)
+          ctx.restore()
+          setCtx(ctx)
         }
     }
     reader.readAsDataURL(file)
+  }
+
+  const handleCopy = () => {
+    const view = document.querySelector('.canvas')
+    const data = view.toDataURL()
+    const copyText = `<img src='${data}' />`
+    const clipboard = new Clipboard('.copy', {
+      text: () => copyText
+    })
+    clipboard.on('success', () => {
+      alert("コピーしました")
+    })
   }
 
   return (
     <>
       <div className="file-upload">
         <input onChange={handleUploadImage} type="file" accept=".jpg,.png" />
+        <button className="copy" onClick={handleCopy}>Copy</button>
       </div>
       <canvas className="canvas"></canvas>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
