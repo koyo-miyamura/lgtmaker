91ab9895e88434ed71b4efb901cb2a311bd699dd
koyo-miyamura <nexus9071@gmail.com>
2020-02-14 02:18:14 +0900

    縁取りしたり複数回アップロードしたり

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ function App() {
   const [ctx, setCtx] = useState(null);
   const [isShowImage, setIsShowImage] = useState(false);
 
-  const fontSizePx = 80;
+  const fontSizePx = 100;
   const fontColor = "#FFFFFF";
+  const strokeColor = "#000000";
+  const lgtmText = "L G T M";
 
   useEffect(() => {
     const view = document.querySelector(".canvas");
@@ -17,30 +19,48 @@ function App() {
   const handleUploadImage = e => {
     const view = document.querySelector(".canvas");
     ctx.clearRect(0, 0, view.width, view.height);
+
     const reader = new FileReader();
     const file = e.target.files[0];
     reader.onloadend = () => {
       const image = new Image();
       image.src = reader.result;
       image.onload = () => {
+        // imageの大きさに合わせてcanvasのリサイズ
         view.width = image.width;
         view.height = image.height;
+
+        // jpeg出力だと背景色が黒になるので、あらかじめ白で塗りつぶす
+        ctx.save();
+        ctx.fillStyle = "#FFFFFF";
+        ctx.fillRect(0, 0, image.width, image.height);
+        ctx.restore();
         ctx.drawImage(image, 0, 0, image.width, image.height);
+
+        // LGTM 重ねる
         ctx.save();
         ctx.font = `bolder ${fontSizePx}px 'MS Pゴシック'`;
         ctx.textAlign = "center";
         ctx.fillStyle = fontColor;
-        ctx.fillText("L G T M", image.width / 2, image.height / 2);
+        ctx.fillText(lgtmText, image.width / 2, image.height / 2);
+        ctx.lineWidth = 1.5;
+        ctx.strokeStyle = strokeColor;
+        ctx.strokeText(lgtmText, image.width / 2, image.height / 2);
         ctx.restore();
 
+        // canvasのままだとコピーなどができないので、img要素として出力
         const data = view.toDataURL("image/jpeg");
         const outputImage = new Image();
         outputImage.src = data;
         outputImage.onload = () => {
           setIsShowImage(true);
-          document.querySelector(".result").appendChild(outputImage);
+          const node = document.querySelector(".result");
+          if (!node.firstChild) {
+            node.appendChild(outputImage);
+          } else {
+            node.replaceChild(outputImage, node.firstChild);
+          }
 
-          const data = view.toDataURL("image/jpeg");
           const copyText = `<img src='${data}' />`;
           const clipboard = new Clipboard(".copy", {
             text: () => copyText
